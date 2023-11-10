cac6045d11d52d0316f27bfb8880021753e75c2c
Hoo-sung <gntjd135@gmail.com>
2023-11-10 17:15:47 +0900

    feat(Order): Order에 OrderItem을 추가하는 addOrderItem 메소드 작성

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -6,7 +6,11 @@ public class Order {
 
     private List<OrderItem> orderItems;
 
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+    }
     public List<OrderItem> getOrderItems() {
         return orderItems;
     }
+
 }
