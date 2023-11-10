d18f3daf758b7204d4316b07a7c45e5e640a06e7
Hoo-sung <gntjd135@gmail.com>
2023-11-10 19:54:37 +0900

    feat(PlannerSystem): 컨트롤러 클래스 생성

---
+++ src/main/java/christmas/controller/PlannerSystem.java
@@ -0,0 +1,19 @@
+package christmas.controller;
+
+import christmas.domain.Day;
+import christmas.domain.Order;
+
+public class PlannerSystem {
+
+    private Day day;
+    private Order order;
+
+    public void run(){
+        initialize();
+
+    }
+
+    private void initialize(){
+
+    }
+}
