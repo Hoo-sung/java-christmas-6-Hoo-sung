d1dc1068527ccb5e7ecfc9ff49606de8e70b929c
Hoo-sung <gntjd135@gmail.com>
2023-11-10 16:03:46 +0900

    feat(MenuItem): MenuItem 클래스 생성

---
+++ src/main/java/christmas/domain/MenuItem.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+public class MenuItem {
+
+    private final String name;
+    private final int price;
+    private final MenuType type;
+
+    MenuItem(String name, int price, MenuType type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public MenuType getType() {
+        return type;
+    }
+}
