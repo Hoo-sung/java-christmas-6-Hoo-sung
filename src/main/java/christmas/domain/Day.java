a6af3d40c5c92b4ba74eb77ca148ad42832891fc
Hoo-sung <gntjd135@gmail.com>
2023-11-10 14:36:28 +0900

    feat(Day): 날짜가 평일인지, 주말인지 체크하는 메소드 작성

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -8,4 +8,14 @@ public class Day {
         this.day = day;
     }
 
+    public static boolean checkWeekend(Day day){
+        if(day.day % 7 ==1 || day.day % 7 == 2)
+            return true;
+        return false;
+    }
+    public static boolean checkWeekday(Day day){
+        if(!checkWeekend(day))
+            return true;
+        return false;
+    }
 }
