19c745a25d1f1d4a863d0577ddd839c3faf813a2
Hoo-sung <gntjd135@gmail.com>
2023-11-10 16:34:58 +0900

    feat(Order): 사용자의 주문을 저장하는 Order 클래스 생성

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+import java.util.List;
+
+public class Order {
+
+    private List<OrderItem> orderItems;
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+}
