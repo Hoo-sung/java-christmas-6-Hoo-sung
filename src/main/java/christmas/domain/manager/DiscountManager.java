668e3679cc9d2b019a6c348e5eaaadd709456a15
Hoo-sung <gntjd135@gmail.com>
2023-11-10 21:04:15 +0900

    feat(DiscountManager): 크리스마스 디데이 할인금액 계산하는 메소드 작성

--- src/main/java/christmas/domain/manager/DiscountManager.java
+++ src/main/java/christmas/domain/manager/DiscountManager.java
@@ -1,7 +1,16 @@
 package christmas.domain.manager;
 
 
+import christmas.domain.Day;
+
 public class DiscountManager {
 
-
+    public static int DDayDiscount(Day day){
+        if(DayManager.isChristmasSeason(day))
+            return calculateDDayDiscount(day);
+        return 0;
+    }
+    private static int calculateDDayDiscount(Day day){
+        return 1000 + 100 * (day.getDay()-1);
+    }
 }
