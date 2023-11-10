f353e27b653c133bc1e67f70661ecc021d27edc0
Hoo-sung <gntjd135@gmail.com>
2023-11-11 01:58:04 +0900

    feat(OutputView): 증정 메뉴를 출력하는 메소드 작성

--- src/main/java/christmas/domain/manager/BonusEventManager.java
+++ src/main/java/christmas/domain/manager/BonusEventManager.java
@@ -4,7 +4,7 @@ import christmas.domain.BonusMenu;
 
 public class BonusEventManager {
 
-    private static final int BONUS_MINIMUM_THRESHOLD = 120000;
+    public static final int BONUS_MINIMUM_THRESHOLD = 120000;
     public static int makeBonusEventDiscount(int originalTotalAmount){
         if(originalTotalAmount < BONUS_MINIMUM_THRESHOLD)
             return 0;
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,10 @@
 package christmas.view;
 
+import christmas.domain.BonusItem;
+import christmas.domain.BonusMenu;
 import christmas.domain.Order;
 import christmas.domain.OrderItem;
+import christmas.domain.manager.BonusEventManager;
 
 public final class OutputView {
 
@@ -25,6 +28,19 @@ public final class OutputView {
     public static void printOriginalTotalAmount(int originalTotalAmount){
         printMessage("<할인 전 총주문 금액>");
         printMessage(originalTotalAmount+"원");
+        printEmptyLine();
+    }
+
+    public static void printBonusMenu(int originalTotalAmount){
+        printMessage("<증정 메뉴>");
+        if(originalTotalAmount < BonusEventManager.BONUS_MINIMUM_THRESHOLD) {
+            printMessage("없음");
+            printEmptyLine();
+            return;
+        }
+        for(BonusMenu bonusMenu : BonusMenu.values()){
+            printMessage(bonusMenu.name()+" "+bonusMenu.getQuantity()+"개");
+        }
         printEmptyLine();
     }
 
