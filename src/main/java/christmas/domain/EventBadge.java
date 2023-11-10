d5ead8ea533e247bbf717f24d1fa001d0a9d1a8e
Hoo-sung <gntjd135@gmail.com>
2023-11-10 15:11:46 +0900

    feat(EventBadge): 이벤트 배지 ENUM 생성

---
+++ src/main/java/christmas/domain/EventBadge.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public enum EventBadge {
+
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타");
+
+    private final String name;
+
+    EventBadge(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
