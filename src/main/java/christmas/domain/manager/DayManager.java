2a5cd63ee1d670d9b79e945dfcb9a9ace733f646
Hoo-sung <gntjd135@gmail.com>
2023-11-10 15:22:32 +0900

    refactor(DayManager): 메소드명 수정 (check... -> is...)

--- src/main/java/christmas/domain/manager/DayManager.java
+++ src/main/java/christmas/domain/manager/DayManager.java
@@ -6,19 +6,19 @@ import static christmas.domain.Day.*;
 
 
 public class DayManager {
-    public static boolean checkWeekend(Day day) {
+    public static boolean isWeekend(Day day) {
         if (isFriday(day) || isSaturday(day))
             return true;
         return false;
     }
 
-    public static boolean checkWeekday(Day day) {
+    public static boolean isWeekday(Day day) {
         if (!(isFriday(day) || isSaturday(day)))
             return true;
         return false;
     }
 
-    public static boolean checkHasStar(Day day) {
+    public static boolean hasStar(Day day) {
         if (isSunday(day) || isChristmas(day))
             return true;
         return false;
