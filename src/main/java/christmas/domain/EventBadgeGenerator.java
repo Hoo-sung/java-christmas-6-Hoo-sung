989c55dede2b31aa75cd1abd432a5ce307cf11a1
Hoo-sung <gntjd135@gmail.com>
2023-11-10 15:42:29 +0900

    feat(EventBadgeGenerator): 클래스 생성 및 createBadge 메소드 작성

---
+++ src/main/java/christmas/domain/EventBadgeGenerator.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public final class EventBadgeGenerator {
+
+    private static final int SANTA_THRESHOLD = 20000;
+    private static final int TREE_THRESHOLD = 10000;
+    private static final int STAR_THRESHOLD = 5000;
+
+    public static EventBadge createBadge(int totalBenefitAmount){
+        if(totalBenefitAmount >= SANTA_THRESHOLD)
+            return EventBadge.SANTA;
+        else if(totalBenefitAmount >=TREE_THRESHOLD)
+            return EventBadge.TREE;
+        else if(totalBenefitAmount >= STAR_THRESHOLD)
+            return EventBadge.STAR;
+        return null;
+    }
+
+}
