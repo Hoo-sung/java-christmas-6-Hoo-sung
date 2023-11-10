d1ac53c23bd17743ee2ffa304b3a01f7cf8c339f
Hoo-sung <gntjd135@gmail.com>
2023-11-10 17:34:56 +0900

    fix(Menu): 메뉴 이름 수정(공백 제거)

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -4,19 +4,19 @@ public enum Menu {
 
     MUSHROOM_SOUP(new MenuItem("양송이수프",6000,MenuType.APPETIZER)),
     TAPAS(new MenuItem("타파스", 5500, MenuType.APPETIZER)),
-    CAESAR_SALAD(new MenuItem("시저 샐러드", 8000, MenuType.APPETIZER)),
+    CAESAR_SALAD(new MenuItem("시저샐러드", 8000, MenuType.APPETIZER)),
 
-    T_BONE_STEAK(new MenuItem("티본 스테이크", 55000, MenuType.MAIN)),
-    BBQ_RIBS(new MenuItem("바비큐 리브", 54000, MenuType.MAIN)),
-    SEAFOOD_PASTA(new MenuItem("해산물 파스타", 35000, MenuType.MAIN)),
-    CHRISTMAS_PASTA(new MenuItem("크리스마스 파스타", 25000, MenuType.MAIN)),
+    T_BONE_STEAK(new MenuItem("티본스테이크", 55000, MenuType.MAIN)),
+    BBQ_RIBS(new MenuItem("바비큐립", 54000, MenuType.MAIN)),
+    SEAFOOD_PASTA(new MenuItem("해산물파스타", 35000, MenuType.MAIN)),
+    CHRISTMAS_PASTA(new MenuItem("크리스마스파스타", 25000, MenuType.MAIN)),
 
-    CHOCOLATE_CAKE(new MenuItem("초코 케이크", 15000, MenuType.DESSERT)),
+    CHOCOLATE_CAKE(new MenuItem("초코케이크", 15000, MenuType.DESSERT)),
     ICE_CREAM(new MenuItem("아이스크림", 5000, MenuType.DESSERT)),
 
 
-    ZERO_COLA(new MenuItem("제로 콜라", 3000, MenuType.BEVERAGE)),
-    RED_WINE(new MenuItem("레드 와인", 60000, MenuType.BEVERAGE)),
+    ZERO_COLA(new MenuItem("제로콜라", 3000, MenuType.BEVERAGE)),
+    RED_WINE(new MenuItem("레드와인", 60000, MenuType.BEVERAGE)),
     CHAMPAGNE(new MenuItem("샴페인", 25000, MenuType.BEVERAGE));
 
     private final MenuItem menuItem;
