495f96b5292734c93a50e7268481748af495f1cd
Hoo-sung <gntjd135@gmail.com>
2023-11-10 14:48:44 +0900

    feat(Day): 해당 날짜가 별을 가지는지 확인하는 checkHasStar 메소드 작성

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -8,26 +8,46 @@ public class Day {
         this.day = day;
     }
 
-    public static boolean checkWeekend(Day day){
-        if(isFriday(day) || isSaturday(day))
-            return true;
-        return false;
-    }
-    public static boolean checkWeekday(Day day){
-        if( ! (isFriday(day) || isSaturday(day)))
+    public static boolean checkWeekend(Day day) {
+        if (isFriday(day) || isSaturday(day))
             return true;
         return false;
     }
 
-    private static boolean isFriday(Day day){
-        if(day.day % 7 == 1)
+    public static boolean checkWeekday(Day day) {
+        if (!(isFriday(day) || isSaturday(day)))
             return true;
         return false;
     }
 
-    private static boolean isSaturday(Day day){
-        if(day.day % 7 == 2)
+    public static boolean checkHasStar(Day day) {
+        if (isSunday(day) || isChristmas(day))
             return true;
         return false;
     }
+
+    private static boolean isFriday(Day day) {
+        if (day.day % 7 == 1)
+            return true;
+        return false;
+    }
+
+    private static boolean isSaturday(Day day) {
+        if (day.day % 7 == 2)
+            return true;
+        return false;
+    }
+
+    private static boolean isSunday(Day day) {
+        if (day.day % 7 == 3)
+            return true;
+        return false;
+    }
+
+    private static boolean isChristmas(Day day) {
+        if (day.day == 25)
+            return true;
+        return false;
+    }
+
 }
