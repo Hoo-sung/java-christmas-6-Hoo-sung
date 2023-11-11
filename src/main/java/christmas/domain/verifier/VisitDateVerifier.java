4ee8340fa4fdf3a052c2a737f2f409fe096b7e84
Hoo-sung <gntjd135@gmail.com>
2023-11-11 21:52:15 +0900

    feat(VisitDateVerifier): 입력한 값이 숫자인지 체크하는 checkNumeric 메소드 생성

--- src/main/java/christmas/domain/verifier/VisitDateVerifier.java
+++ src/main/java/christmas/domain/verifier/VisitDateVerifier.java
@@ -1,8 +1,18 @@
 package christmas.domain.verifier;
 
+import java.math.BigInteger;
+
 public class VisitDateVerifier implements Verifier {
     @Override
     public void check(String input) {
+        checkNumeric(input);
+    }
 
+    private void checkNumeric(String input){
+        try{
+            new BigInteger(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 입력한 값은 숫자가 아닙니다.");
+        }
     }
 }
