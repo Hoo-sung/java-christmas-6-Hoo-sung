0383910406073e2a2535887e860e44e3e44c448a
Hoo-sung <gntjd135@gmail.com>
2023-11-11 16:05:45 +0900

    refactor(PaymentCalculator): 스트림 사용하여 코드 리펙토링, 변수명 변경

--- src/main/java/christmas/domain/service/PaymentCalculator.java
+++ src/main/java/christmas/domain/service/PaymentCalculator.java
@@ -2,22 +2,19 @@ package christmas.domain.service;
 
 import christmas.domain.DiscountRecord;
 import christmas.domain.Order;
-import christmas.domain.OrderItem;
-import christmas.system.Constant;
+
 
 public class PaymentCalculator {
 
     public static int calculateOriginalTotal(Order order){
-        int total = Constant.ZERO;
-        for (OrderItem orderItem : order.getOrderItems()) {
-            total += (orderItem.getPrice() * orderItem.getQuantity());
-        }
-        return total;
+        return order.getOrderItems().stream()
+                .mapToInt(orderItem -> orderItem.getPrice() * orderItem.getQuantity())
+                .sum();
     }
 
     public static int calculateTotalWithDiscount(int originalTotalAmount, DiscountRecord discountRecord){
-        int minusValue = discountRecord.getdDayDiscountAmount() + discountRecord.getWeekdayDiscountAmount()
+        int discountTotal = discountRecord.getdDayDiscountAmount() + discountRecord.getWeekdayDiscountAmount()
                 + discountRecord.getWeekendDiscountAmount() + discountRecord.getStarDayDiscountAmount();
-        return originalTotalAmount - minusValue;
+        return originalTotalAmount - discountTotal;
     }
 }
