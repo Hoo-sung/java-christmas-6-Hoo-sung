0c36d4cfef89ffac8eb4e562e84cc822bbc8a429
Hoo-sung <gntjd135@gmail.com>
2023-11-12 00:04:55 +0900

    refactor(OrderMenuVerifier): 메서드가 호출되는 순서 변경

--- src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
+++ src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
@@ -8,13 +8,13 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class OrderMenuVerifier implements Verifier{
+public class OrderMenuVerifier implements Verifier {
     @Override
     public void check(String input) {
         checkMenuFormat(input);
-        checkMenuQuantity(input);
-        checkDistinctMenu(input);
         checkMenuExistence(input);
+        checkDistinctMenu(input);
+        checkMenuQuantity(input);
     }
 
     private void checkMenuFormat(String input) {
@@ -31,34 +31,33 @@ public class OrderMenuVerifier implements Verifier{
         }
     }
 
-    private void checkMenuQuantity(String input){
+    private void checkMenuExistence(String input) {
         String[] orders = input.split(",");
-        for(String order : orders){
+        for (String order : orders) {
             String[] orderInfo = order.split("-");
-            int quantity = Integer.parseInt(orderInfo[1]);
-            if(quantity < 1)
+            if (Menu.getMenuItemByName(orderInfo[0]) == null)
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    private void checkDistinctMenu(String input){
+    private void checkDistinctMenu(String input) {
         Set<String> uniqueMenuItems = new HashSet<>();
         String[] orders = input.split(",");
-        for(String order : orders){
+        for (String order : orders) {
             String[] orderInfo = order.split("-");
-            if(!uniqueMenuItems.add(orderInfo[0]))
+            if (!uniqueMenuItems.add(orderInfo[0]))
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    private void checkMenuExistence(String input){
+    private void checkMenuQuantity(String input) {
         String[] orders = input.split(",");
-        for(String order : orders){
+        for (String order : orders) {
             String[] orderInfo = order.split("-");
-            if(Menu.getMenuItemByName(orderInfo[0])== null)
+            int quantity = Integer.parseInt(orderInfo[1]);
+            if (quantity < 1)
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
-
 
 }
