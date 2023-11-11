3572d764d27e61acd14a92243fff6716673fbe96
Hoo-sung <gntjd135@gmail.com>
2023-11-11 22:06:13 +0900

    feat(InputView): VisiitDateVerifier 클래스 의준성 주입

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Day;
 import christmas.domain.Order;
 import christmas.domain.util.Util;
+import christmas.domain.verifier.Verifier;
+import christmas.domain.verifier.VisitDateVerifier;
 
 import static christmas.system.IOMessage.ORDER_PROMPT_MESSAGE;
 import static christmas.system.IOMessage.VISIT_DATE_PROMPT_MESSAGE;
@@ -13,15 +15,17 @@ public final class InputView {
 
     }
 
+    private static final Verifier visitDateVerifier = new VisitDateVerifier();
+
     public static Day readDay() {
         while (true) {
             try {
                 OutputView.printMessage(VISIT_DATE_PROMPT_MESSAGE);
                 String input = Console.readLine();
-                //검증 부분
+                visitDateVerifier.check(input);
                 return new Day(Integer.parseInt(input));
             } catch (IllegalArgumentException e) {
-
+                OutputView.printMessage(e.getMessage());
             }
         }
     }
