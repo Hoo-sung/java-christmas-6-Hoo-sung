6a3353626d664dc1c29d32dad07dad3bd9982c23
Hoo-sung <gntjd135@gmail.com>
2023-11-11 22:27:47 +0900

    feat(OrderMenuVerifier): 메뉴 형식을 검증하는 checkMenuFormat 메소드 생성

--- src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
+++ src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
@@ -1,8 +1,24 @@
 package christmas.domain.verifier;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OrderMenuVerifier implements Verifier{
     @Override
     public void check(String input) {
-
+        checkMenuFormat(input);
     }
+
+    private void checkMenuFormat(String input){
+        String format = "[가-힣]+-\\d+";
+        Pattern pattern = Pattern.compile(format);
+        Matcher matcher = pattern.matcher(input);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+
 }
