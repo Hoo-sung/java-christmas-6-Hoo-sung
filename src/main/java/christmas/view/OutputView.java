cf253d22672555e44b5443ae5a2466b2dc527a17
Hoo-sung <gntjd135@gmail.com>
2023-11-11 14:58:22 +0900

    refactor(OuputView): static import문으로 가독성 향상

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,10 @@
 package christmas.view;
 
 import christmas.domain.*;
-import christmas.domain.manager.BonusEventManager;
-import christmas.domain.util.Util;
-import christmas.system.IOMessage;
+
+import static christmas.domain.manager.BonusEventManager.*;
+import static christmas.domain.util.Util.*;
+import static christmas.system.IOMessage.*;
 
 public final class OutputView {
 
@@ -17,71 +18,71 @@ public final class OutputView {
     public static void printOrderList(Order order){
         printMessage("<주문 메뉴>");
         for(OrderItem orderItem: order.getOrderItems()){
-            printMessage(orderItem.getName()+" "+orderItem.getQuantity()+IOMessage.QUANTITY_UNIT);
+            printMessage(orderItem.getName()+" "+orderItem.getQuantity()+ QUANTITY_UNIT);
         }
         printEmptyLine();
     }
 
     public static void printOriginalTotalAmount(int originalTotalAmount){
         printMessage("<할인 전 총주문 금액>");
-        printMessage(Util.createFormattedAmount(originalTotalAmount)+IOMessage.MONEY_UNIT);
+        printMessage(createFormattedAmount(originalTotalAmount)+ MONEY_UNIT);
         printEmptyLine();
     }
 
     public static void printBonusMenu(int originalTotalAmount){
         printMessage("<증정 메뉴>");
-        if(originalTotalAmount < BonusEventManager.BONUS_MINIMUM_THRESHOLD) {
-            printMessage(IOMessage.NONE);
+        if(originalTotalAmount < BONUS_MINIMUM_THRESHOLD) {
+            printMessage(NONE);
             printEmptyLine();
             return;
         }
         for(BonusMenu bonusMenu : BonusMenu.values()){
-            printMessage(bonusMenu.getName()+" "+Util.createFormattedAmount(bonusMenu.getQuantity())+IOMessage.QUANTITY_UNIT);
+            printMessage(bonusMenu.getName()+" "+ createFormattedAmount(bonusMenu.getQuantity())+ QUANTITY_UNIT);
         }
         printEmptyLine();
     }
 
     public static void printDiscountRecord(DiscountRecord discountRecord){
         printMessage("<혜택 내역>");
-        if(discountRecord.getTotalDiscountAmount()==IOMessage.ZERO){
-            printMessage(IOMessage.NONE);
+        if(discountRecord.getTotalDiscountAmount()== ZERO){
+            printMessage(NONE);
             printEmptyLine();
             return;
         }
-        if(discountRecord.getdDayDiscountAmount() !=IOMessage.ZERO )
-            printMessage("크리크마스 디데이 할인: -"+Util.createFormattedAmount(discountRecord.getdDayDiscountAmount())+IOMessage.MONEY_UNIT);
-        if(discountRecord.getWeekdayDiscountAmount()!=IOMessage.ZERO)
-            printMessage("평일 할인: -"+Util.createFormattedAmount(discountRecord.getWeekdayDiscountAmount())+IOMessage.MONEY_UNIT);
-        if(discountRecord.getWeekendDiscountAmount()!=IOMessage.ZERO)
-            printMessage("주말 할인: -"+Util.createFormattedAmount(discountRecord.getWeekendDiscountAmount())+IOMessage.MONEY_UNIT);
-        if(discountRecord.getStarDayDiscountAmount()!=IOMessage.ZERO)
-            printMessage("특별 할인: -"+Util.createFormattedAmount(discountRecord.getStarDayDiscountAmount())+IOMessage.MONEY_UNIT);
-        if(discountRecord.getBonusEventDiscount()!=IOMessage.ZERO)
-            printMessage("증정 이벤트: -"+Util.createFormattedAmount(discountRecord.getBonusEventDiscount())+IOMessage.MONEY_UNIT);
+        if(discountRecord.getdDayDiscountAmount() != ZERO )
+            printMessage("크리크마스 디데이 할인: -"+ createFormattedAmount(discountRecord.getdDayDiscountAmount())+ MONEY_UNIT);
+        if(discountRecord.getWeekdayDiscountAmount()!= ZERO)
+            printMessage("평일 할인: -"+ createFormattedAmount(discountRecord.getWeekdayDiscountAmount())+ MONEY_UNIT);
+        if(discountRecord.getWeekendDiscountAmount()!= ZERO)
+            printMessage("주말 할인: -"+ createFormattedAmount(discountRecord.getWeekendDiscountAmount())+ MONEY_UNIT);
+        if(discountRecord.getStarDayDiscountAmount()!= ZERO)
+            printMessage("특별 할인: -"+ createFormattedAmount(discountRecord.getStarDayDiscountAmount())+ MONEY_UNIT);
+        if(discountRecord.getBonusEventDiscount()!= ZERO)
+            printMessage("증정 이벤트: -"+ createFormattedAmount(discountRecord.getBonusEventDiscount())+ MONEY_UNIT);
         printEmptyLine();
     }
 
     public  static void printTotalDiscountAmount(DiscountRecord discountRecord){
         printMessage("<총혜택 금액>");
-        if(discountRecord.getTotalDiscountAmount() == IOMessage.ZERO){
-            printMessage(IOMessage.ZERO+IOMessage.MONEY_UNIT);
+        if(discountRecord.getTotalDiscountAmount() == ZERO){
+            printMessage(ZERO+ MONEY_UNIT);
             printEmptyLine();
             return;
         }
-        printMessage("-"+Util.createFormattedAmount(discountRecord.getTotalDiscountAmount())+IOMessage.MONEY_UNIT);
+        printMessage("-"+ createFormattedAmount(discountRecord.getTotalDiscountAmount())+ MONEY_UNIT);
         printEmptyLine();
     }
 
     public static void printExpectedPayment(int expectedPayment){
         printMessage("<할인 후 예상 결제 금액>");
-        printMessage(Util.createFormattedAmount(expectedPayment)+IOMessage.MONEY_UNIT);
+        printMessage(createFormattedAmount(expectedPayment)+ MONEY_UNIT);
         printEmptyLine();
     }
 
     public static void printEventBadge(EventBadge badge){
         printMessage("<12월 이벤트 배지>");
         if(badge == null){
-            printMessage(IOMessage.NONE);
+            printMessage(NONE);
             return;
         }
         printMessage(badge.getName());
