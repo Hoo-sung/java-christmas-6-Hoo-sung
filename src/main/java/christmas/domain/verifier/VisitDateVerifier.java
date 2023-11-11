cd2b050810ff296121ddfb6c259e1c0efd00bbe2
Hoo-sung <gntjd135@gmail.com>
2023-11-11 21:59:53 +0900

    feat(VisitDateVerifier): 유효한 범위인지 체크하는 checkRange 메소드 생성

--- src/main/java/christmas/domain/verifier/VisitDateVerifier.java
+++ src/main/java/christmas/domain/verifier/VisitDateVerifier.java
@@ -6,6 +6,7 @@ public class VisitDateVerifier implements Verifier {
     @Override
     public void check(String input) {
         checkNumeric(input);
+        checkRange(input);
     }
 
     private void checkNumeric(String input){
@@ -14,5 +15,11 @@ public class VisitDateVerifier implements Verifier {
         }catch(NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 입력한 값은 숫자가 아닙니다.");
         }
+    }
+
+    private void checkRange(String input){
+        BigInteger inputnum = new BigInteger(input);
+        if(inputnum.compareTo(BigInteger.ONE) < 0 || inputnum.compareTo(BigInteger.valueOf(31)) > 0)
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 }
