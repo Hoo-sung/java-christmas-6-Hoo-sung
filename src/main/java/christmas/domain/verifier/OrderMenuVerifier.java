2eb0a76608e004e9d228752a28a170d3bd26dff8
Hoo-sung <gntjd135@gmail.com>
2023-11-11 23:05:35 +0900

    feat(OrderMenuVerifier): 메뉴판에 존재하는 메뉴인지 검증하는 메소드 생성

--- src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
+++ src/main/java/christmas/domain/verifier/OrderMenuVerifier.java
@@ -1,6 +1,11 @@
 package christmas.domain.verifier;
 
 
+import christmas.domain.Menu;
+import christmas.domain.MenuItem;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +14,8 @@ public class OrderMenuVerifier implements Verifier{
     public void check(String input) {
         checkMenuFormat(input);
         checkMenuQuantity(input);
+        checkDistinctMenu(input);
+        checkMenuExistence(input);
     }
 
     private void checkMenuFormat(String input){
@@ -28,6 +35,25 @@ public class OrderMenuVerifier implements Verifier{
             check(orderInfo[1]);
             int quantity = Integer.parseInt(orderInfo[1]);
             if(quantity < 1)
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void checkDistinctMenu(String input){
+        Set<String> uniqueMenuItems = new HashSet<>();
+        String[] orders = input.split(",");
+        for(String order : orders){
+            String[] orderInfo = order.split("-");
+            if(!uniqueMenuItems.add(orderInfo[0]))
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void checkMenuExistence(String input){
+        String[] orders = input.split(",");
+        for(String order : orders){
+            String[] orderInfo = order.split("-");
+            if(Menu.getMenuItemByName(orderInfo[0])== null)
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
