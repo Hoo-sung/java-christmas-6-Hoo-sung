d6fa9e6c3920d78a9aecf11ef19dedb22f715354
Hoo-sung <gntjd135@gmail.com>
2023-11-12 09:43:49 +0900

    feat(RuntimeVerifier): 음료만 주문했는지 체크하는 메소드 생성

--- src/main/java/christmas/domain/verifier/RuntimeVerifier.java
+++ src/main/java/christmas/domain/verifier/RuntimeVerifier.java
@@ -1,10 +1,21 @@
 package christmas.domain.verifier;
 
+import christmas.domain.MenuType;
 import christmas.domain.Order;
+import christmas.domain.OrderItem;
+import christmas.system.ExceptionMessage;
 
 public class RuntimeVerifier {
 
     public void check(Order order){
+        checkBeverageOnly(order);
+    }
 
+    private void checkBeverageOnly(Order order){
+        for(OrderItem orderItem : order.getOrderItems()){
+            if(orderItem.getMenuType() != MenuType.BEVERAGE)
+                return;
+        }
+        throw new IllegalStateException(ExceptionMessage.BEVERAGE_ONLY_ORDER_MESSAGE);
     }
 }
