f9e8ace7f52beeb93d068d1fb042c56964faeb52
Hoo-sung <gntjd135@gmail.com>
2023-11-13 16:57:12 +0900

    test(DiscountManager): 평일 할인 계산하는 메소드 테스트 코드 작성

--- src/test/java/christmas/domain/manager/DiscountManagerTest.java
+++ src/test/java/christmas/domain/manager/DiscountManagerTest.java
@@ -1,6 +1,8 @@
 package christmas.domain.manager;
 
 import christmas.domain.Day;
+import christmas.domain.Order;
+import christmas.domain.util.Util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,5 +27,19 @@ class DiscountManagerTest {
         Day day = new Day(input);
         assertThat(discountManager.getDDayDiscount(day)).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @DisplayName("getWeekDayDiscount 메소드 테스트")
+    @CsvSource(value = {
+            "26:타파스-1,제로콜라-1:0",
+            "3:티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:4046",
+            "21:초코케이크-2,아이스크림-3:10115"
+    },delimiter = ':')
+    void 평일_할인_금액_테스트(int dayInput, String orderInput, int expected) {
+        Day day = new Day(dayInput);
+        Order order = Util.createOrderFromInput(orderInput);
+        assertThat(discountManager.getWeekDayDiscount(day,order)).isEqualTo(expected);
+    }
+
 
 }
