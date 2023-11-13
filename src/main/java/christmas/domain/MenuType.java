59febd8958312d0cf2054d3be17d6d81c9c6a46d
Hoo-sung <gntjd135@gmail.com>
2023-11-13 23:23:53 +0900

    refactor(MenuType): 사용하지 않는 메소드 제거

--- src/main/java/christmas/domain/MenuType.java
+++ src/main/java/christmas/domain/MenuType.java
@@ -1,18 +1,8 @@
 package christmas.domain;
 
 public enum MenuType {
-    APPETIZER("애피타이저"),
-    MAIN("메인"),
-    DESSERT("디저트"),
-    BEVERAGE("음료");
-
-    private final String menuType;
-
-    MenuType(String menuType) {
-        this.menuType = menuType;
-    }
-
-    public String getMenuType() {
-        return menuType;
-    }
+    APPETIZER,
+    MAIN,
+    DESSERT,
+    BEVERAGE;
 }
