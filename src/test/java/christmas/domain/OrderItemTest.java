e39bd37879ce306fb672cf9cc8c305f0d1f7f84a
Hoo-sung <gntjd135@gmail.com>
2023-11-13 23:35:47 +0900

    test(OrderItem): getMenuType 메소드 테스트 코드 작성

--- src/test/java/christmas/domain/OrderItemTest.java
+++ src/test/java/christmas/domain/OrderItemTest.java
@@ -41,4 +41,20 @@ class OrderItemTest {
         }
     }
 
+    @Nested
+    class GetMenuType {
+        @ParameterizedTest
+        @DisplayName("getMenuType 메소드 테스트")
+        @CsvSource(value = {
+                "샴페인, 25000, BEVERAGE, 4, BEVERAGE",
+                "아이스크림, 5000, DESSERT, 3, DESSERT",
+                "시저샐러드, 8000, APPETIZER, 7, APPETIZER",
+        })
+        void 주문한_메뉴_타입이_잘_반환되는지_테스트(String name, int price, MenuType menuType, int quantity, MenuType expected) {
+            MenuItem menuItem = new MenuItem(name, price, menuType);
+            OrderItem orderItem = new OrderItem(menuItem, quantity);
+            assertThat(orderItem.getMenuType()).isEqualTo(expected);
+        }
+    }
+
 }
