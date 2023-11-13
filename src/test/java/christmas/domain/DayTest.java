d1f5afd8941e3ee0390bc1116ec8655e0437bc65
Hoo-sung <gntjd135@gmail.com>
2023-11-13 21:28:20 +0900

    test(Day): 평일 할인을 받는지 체크하는 메소드 테스트 코드 작성

--- src/test/java/christmas/domain/DayTest.java
+++ src/test/java/christmas/domain/DayTest.java
@@ -20,4 +20,15 @@ class DayTest {
         }
     }
 
+    @Nested
+    class IsWeekday {
+        @ParameterizedTest
+        @DisplayName("isWeekday 메소드 테스트")
+        @CsvSource(value = {"1:false", "7:true", "24:true", "25:true", "27:true","31:true"}, delimiter = ':')
+        void 평일_할인을_받는지_테스트(int dayNum, boolean expected) {
+            Day day = new Day(dayNum);
+            assertThat(day.isWeekday()).isEqualTo(expected);
+        }
+    }
+
 }
