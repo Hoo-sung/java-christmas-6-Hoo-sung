f2c24144653c153ac5dd3b38d486c9a6fea042bb
Hoo-sung <gntjd135@gmail.com>
2023-11-13 17:06:42 +0900

    test(DiscountManager): 특별 할인 계산하는 메소드 테스트 코드 작성

--- src/test/java/christmas/domain/manager/DiscountManagerTest.java
+++ src/test/java/christmas/domain/manager/DiscountManagerTest.java
@@ -28,18 +28,20 @@ class DiscountManagerTest {
         assertThat(discountManager.getDDayDiscount(day)).isEqualTo(expected);
     }
 
+
     @ParameterizedTest
     @DisplayName("getWeekDayDiscount 메소드 테스트")
     @CsvSource(value = {
             "26:타파스-1,제로콜라-1:0",
             "3:티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:4046",
             "21:초코케이크-2,아이스크림-3:10115"
-    },delimiter = ':')
+    }, delimiter = ':')
     void 평일_할인_금액_테스트(int dayInput, String orderInput, int expected) {
         Day day = new Day(dayInput);
         Order order = Util.createOrderFromInput(orderInput);
-        assertThat(discountManager.getWeekDayDiscount(day,order)).isEqualTo(expected);
+        assertThat(discountManager.getWeekDayDiscount(day, order)).isEqualTo(expected);
     }
+
 
     @ParameterizedTest
     @DisplayName("getWeekendDiscount 메소드 테스트")
@@ -47,12 +49,25 @@ class DiscountManagerTest {
             "26:타파스-1,제로콜라-1:0",
             "3:티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:0",
             "22:바비큐립-2,해산물파스타-1,샴페인-3:6069"
-    },delimiter = ':')
+    }, delimiter = ':')
     void 주말_할인_금액_테스트(int dayInput, String orderInput, int expected) {
         Day day = new Day(dayInput);
         Order order = Util.createOrderFromInput(orderInput);
-        assertThat(discountManager.getWeekendDiscount(day,order)).isEqualTo(expected);
+        assertThat(discountManager.getWeekendDiscount(day, order)).isEqualTo(expected);
     }
 
+
+    @ParameterizedTest
+    @DisplayName("getStarDayDiscount 메소드 테스트")
+    @CsvSource(value = {
+            "1:0",
+            "3:1000",
+            "25:1000",
+            "26:0",
+    }, delimiter = ':')
+    void 특별_할인_금액_테스트(int dayInput, int expected) {
+        Day day = new Day(dayInput);
+        assertThat(discountManager.getStarDayDiscount(day)).isEqualTo(expected);
+    }
 
 }
