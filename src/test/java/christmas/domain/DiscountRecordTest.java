3af12aab29e27d2d08ba544163adc830b98836e7
Hoo-sung <gntjd135@gmail.com>
2023-11-14 20:58:14 +0900

    fix(DiscountRecordTest): MockDiscountRecord 클래스 생성하여 테스트 코드 수정

--- src/test/java/christmas/domain/DiscountRecordTest.java
+++ src/test/java/christmas/domain/DiscountRecordTest.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import christmas.domain.manager.BonusEventManager;
 import christmas.domain.manager.DiscountManager;
+import christmas.domain.subclass.MockDiscountRecord;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -27,26 +28,26 @@ class DiscountRecordTest {
         @ParameterizedTest
         @DisplayName("혜택내역의 내용들 잘 반환하는지 메소드 테스트")
         @MethodSource("getTestData")
-        void 혜택내역을_잘_반환하는지_테스트(Day day, Order order, int originalTotalAmount,
+        void 혜택내역을_잘_반환하는지_테스트(Day day, Order order,
                                int expectedDDayDiscountAmount, int expectedWeekdayDiscountAmount, int expectedWeekendDiscountAmount, int expectedStarDayDiscountAmount,
                                int expectedBonusEventBenefit) {
 
-            DiscountRecord discountRecord = DiscountRecord.create(day, order, discountManager, bonusEventManager);
+            MockDiscountRecord mockDiscountRecord = MockDiscountRecord.create(day, order, discountManager, bonusEventManager);
             assertAll(
-                    () -> assertEquals(expectedDDayDiscountAmount, discountRecord.getdDayDiscountAmount()),
-                    () -> assertEquals(expectedWeekdayDiscountAmount, discountRecord.getWeekdayDiscountAmount()),
-                    () -> assertEquals(expectedWeekendDiscountAmount, discountRecord.getWeekendDiscountAmount()),
-                    () -> assertEquals(expectedStarDayDiscountAmount, discountRecord.getSpecialDayDiscountAmount()),
-                    () -> assertEquals(expectedBonusEventBenefit, discountRecord.getBonusEventBenefit())
+                    () -> assertEquals(expectedDDayDiscountAmount, mockDiscountRecord.getdDayDiscountAmount()),
+                    () -> assertEquals(expectedWeekdayDiscountAmount, mockDiscountRecord.getWeekdayDiscountAmount()),
+                    () -> assertEquals(expectedWeekendDiscountAmount, mockDiscountRecord.getWeekendDiscountAmount()),
+                    () -> assertEquals(expectedStarDayDiscountAmount, mockDiscountRecord.getSpecialDayDiscountAmount()),
+                    () -> assertEquals(expectedBonusEventBenefit, mockDiscountRecord.getBonusEventBenefit())
             );
         }
 
         private static Stream<Arguments> getTestData() {
             return Stream.of(
-                    Arguments.of(createDay(5), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 1), 5000, 0, 0, 0, 0, 0),
-                    Arguments.of(createDay(5), createOrderWithItems("샴페인", 25000, MenuType.BEVERAGE, 4), 100000, 1400, 0, 0, 0, 0),
-                    Arguments.of(createDay(7), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 3), 15000, 1600, 6069, 0, 0, 0),
-                    Arguments.of(createDay(25), createOrderWithItems("티본스테이크", 55000, MenuType.MAIN, 5), 275000, 3400, 0, 0, 1000, 25000)
+                    Arguments.of(createDay(5), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 1), 0, 0, 0, 0, 0),
+                    Arguments.of(createDay(5), createOrderWithItems("샴페인", 25000, MenuType.BEVERAGE, 4), 1400, 0, 0, 0, 0),
+                    Arguments.of(createDay(7), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 3), 1600, 6069, 0, 0, 0),
+                    Arguments.of(createDay(25), createOrderWithItems("티본스테이크", 55000, MenuType.MAIN, 5), 3400, 0, 0, 1000, 25000)
             );
         }
     }
@@ -56,7 +57,7 @@ class DiscountRecordTest {
         @ParameterizedTest
         @DisplayName("총 혜택금액을 잘 반환하는지 메소드 테스트")
         @MethodSource("getTestData")
-        void 총혜택_금역을_잘_반환하는지_테스트(Day day, Order order, int originalTotalAmount, int expectedTotalBenefitAmount) {
+        void 총혜택_금역을_잘_반환하는지_테스트(Day day, Order order,int expectedTotalBenefitAmount) {
 
             DiscountRecord discountRecord = DiscountRecord.create(day, order, discountManager, bonusEventManager);
             Assertions.assertThat(discountRecord.getTotalBenefitAmount()).isEqualTo(expectedTotalBenefitAmount);
@@ -64,10 +65,10 @@ class DiscountRecordTest {
 
         private static Stream<Arguments> getTestData() {
             return Stream.of(
-                    Arguments.of(createDay(5), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 1), 5000, 0),
-                    Arguments.of(createDay(5), createOrderWithItems("샴페인", 25000, MenuType.BEVERAGE, 4), 100000, 1400),
-                    Arguments.of(createDay(7), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 3), 15000, 7669),
-                    Arguments.of(createDay(25), createOrderWithItems("티본스테이크", 55000, MenuType.MAIN, 5), 275000, 29400)
+                    Arguments.of(createDay(5), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 1), 0),
+                    Arguments.of(createDay(5), createOrderWithItems("샴페인", 25000, MenuType.BEVERAGE, 4), 1400),
+                    Arguments.of(createDay(7), createOrderWithItems("아이스크림", 5000, MenuType.DESSERT, 3), 7669),
+                    Arguments.of(createDay(25), createOrderWithItems("티본스테이크", 55000, MenuType.MAIN, 5), 29400)
             );
         }
 
---
+++ src/test/java/christmas/domain/subclass/MockDiscountRecord.java
@@ -0,0 +1,63 @@
+package christmas.domain.subclass;
+
+import christmas.domain.Day;
+import christmas.domain.Order;
+import christmas.domain.manager.BonusEventManager;
+import christmas.domain.manager.DiscountManager;
+
+import static christmas.system.Constant.EVENT_THRESHOLD_AMOUNT;
+import static christmas.system.Constant.ZERO;
+
+public class MockDiscountRecord {
+    private final int dDayDiscountAmount;
+    private final int weekdayDiscountAmount;
+    private final int weekendDiscountAmount;
+    private final int specialDayDiscountAmount;
+    private final int bonusEventBenefit;
+
+    private MockDiscountRecord(Day day, Order order,
+                           DiscountManager discountManager, BonusEventManager bonusEventManager) {
+        this.dDayDiscountAmount = discountManager.calculateDDayDiscount(day);
+        this.weekdayDiscountAmount = discountManager.calculateWeekDayDiscount(day, order);
+        this.weekendDiscountAmount = discountManager.calculateWeekendDiscount(day, order);
+        this.specialDayDiscountAmount = discountManager.calculateSpecialDayDiscount(day);
+        this.bonusEventBenefit = bonusEventManager.makeBonusEventBenefit(order);
+    }
+
+    private MockDiscountRecord() {
+        this.dDayDiscountAmount = ZERO;
+        this.weekdayDiscountAmount = ZERO;
+        this.weekendDiscountAmount = ZERO;
+        this.specialDayDiscountAmount = ZERO;
+        this.bonusEventBenefit = ZERO;
+    }
+
+    public static MockDiscountRecord create(Day day, Order order,
+                                        DiscountManager discountManager, BonusEventManager bonusEventManager) {
+        if (order.getTotalOrderAmount() < EVENT_THRESHOLD_AMOUNT)
+            return emptyDiscountRecord();
+        return new MockDiscountRecord(day, order, discountManager, bonusEventManager);
+    }
+    private static MockDiscountRecord emptyDiscountRecord() {
+        return new MockDiscountRecord();
+    }
+    public int getdDayDiscountAmount() {
+        return dDayDiscountAmount;
+    }
+
+    public int getWeekdayDiscountAmount() {
+        return weekdayDiscountAmount;
+    }
+
+    public int getWeekendDiscountAmount() {
+        return weekendDiscountAmount;
+    }
+
+    public int getSpecialDayDiscountAmount() {
+        return specialDayDiscountAmount;
+    }
+
+    public int getBonusEventBenefit() {
+        return bonusEventBenefit;
+    }
+}
