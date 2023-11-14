e6fd20258c90fb0193aa573e53258ce918901c94
Hoo-sung <gntjd135@gmail.com>
2023-11-14 23:34:10 +0900

    refactor(MenuTest): getMenuItemByName 예외 로직 테스트 코드 추가

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MenuTest {
 
@@ -34,13 +35,13 @@ class MenuTest {
     @Nested
     class GetMenuItemByName {
         @ParameterizedTest
-        @DisplayName("getMenuItemByName 테스트")
+        @DisplayName("[정상] getMenuItemByName 테스트")
         @CsvSource({
                 "샴페인, 샴페인, 25000, BEVERAGE",
                 "크리스마스파스타, 크리스마스파스타, 25000, MAIN",
                 "시저샐러드, 시저샐러드, 8000, APPETIZER",
         })
-        void 메뉴_이름으로_메뉴아이템_찾는_테스트(String input, String expectedName, int expectedPrice, MenuType expectedType) {
+        void 메뉴_이름으로_메뉴아이템_찾을_수_있는_정상_테스트(String input, String expectedName, int expectedPrice, MenuType expectedType) {
 
             Optional<MenuItem> menuItemOptional = Menu.getMenuItemByName(input);
             menuItemOptional.ifPresent(menuItem -> {
@@ -48,6 +49,18 @@ class MenuTest {
                 assertThat(menuItem.getPrice()).isEqualTo(expectedPrice);
                 assertThat(menuItem.getType()).isEqualTo(expectedType);
             });
+        }
+
+        @ParameterizedTest
+        @DisplayName("[찾을 수 없는 경우] getMenuItemByName 테스트")
+        @CsvSource({
+                "샴파인",
+                "크라스마스파스타",
+                "시쪄샐러드",
+        })
+        void 메뉴판에_존재하지_않는_메뉴이름인_경우(String input) {
+            Optional<MenuItem> menuItemOptional = Menu.getMenuItemByName(input);
+            assertTrue(menuItemOptional.isEmpty());
         }
     }
 
