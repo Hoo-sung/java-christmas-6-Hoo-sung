72ab51af1c6f695f423cd7795772ccaa1ca4ec7a
Hoo-sung <gntjd135@gmail.com>
2023-11-14 14:49:07 +0900

    refactor(EventBadgeGeneratorTest): 클래스 분리와 메서드 시그니처를 수정하여 테스트 코드 리펙토링

--- src/test/java/christmas/domain/service/EventBadgeGeneratorTest.java
+++ src/test/java/christmas/domain/service/EventBadgeGeneratorTest.java
@@ -1,8 +1,10 @@
 package christmas.domain.service;
 
+import christmas.domain.service.subclass.CreateBadgeTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +21,8 @@ class EventBadgeGeneratorTest {
             "75432:산타"
     }, delimiter = ':')
     void 총_혜택_금액에_따른_이번트_배지_테스트(int totalBenefitAmount, String expected) {
-        assertThat(EventBadgeGenerator.createBadge(totalBenefitAmount).getName()).isEqualTo(expected);
+        CreateBadgeTest createBadgeTest = new CreateBadgeTest();
+        assertThat(createBadgeTest.createBadge(totalBenefitAmount).getName()).isEqualTo(expected);
     }
 
 
---
+++ src/test/java/christmas/domain/service/subclass/CreateBadgeTest.java
@@ -0,0 +1,20 @@
+package christmas.domain.service.subclass;
+
+import christmas.domain.EventBadge;
+
+public class CreateBadgeTest {
+
+    private  final int SANTA_THRESHOLD = 20000;
+    private  final int TREE_THRESHOLD = 10000;
+    private  final int STAR_THRESHOLD = 5000;
+
+    public  EventBadge createBadge(int totalBenefitAmount) {
+        if (totalBenefitAmount >= SANTA_THRESHOLD)
+            return EventBadge.SANTA;
+        else if (totalBenefitAmount >= TREE_THRESHOLD)
+            return EventBadge.TREE;
+        else if (totalBenefitAmount >= STAR_THRESHOLD)
+            return EventBadge.STAR;
+        return EventBadge.NONE;
+    }
+}
