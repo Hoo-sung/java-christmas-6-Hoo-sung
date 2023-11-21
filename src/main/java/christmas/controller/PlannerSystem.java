2798dafbcb4b357b512802e3b4fb947796326764
Hoo-sung <gntjd135@gmail.com>
2023-11-21 10:11:41 +0900

    refactor(PlannerSystem): 함수형 인터페이스를 사용하여 코드의 유연성 향상

--- src/main/java/christmas/controller/PlannerSystem.java
+++ src/main/java/christmas/controller/PlannerSystem.java
@@ -6,6 +6,8 @@ import christmas.domain.verifier.RuntimeVerifier;
 import christmas.domain.verifier.Verifier;
 import christmas.view.InputView;
 
+import java.util.function.Supplier;
+
 import static christmas.system.IOMessage.ORDER_PROMPT_MESSAGE;
 import static christmas.system.IOMessage.VISIT_DATE_PROMPT_MESSAGE;
 import static christmas.view.InputView.*;
@@ -15,22 +17,36 @@ public class PlannerSystem {
 
     private final Day day;
     private final Order order;
-
     private final SettlementSystem settlementSystem;
-
-    private final Verifier<Order> runtimeVerifier = new RuntimeVerifier();
 
     public PlannerSystem() {
         printMessage(VISIT_DATE_PROMPT_MESSAGE);
         this.day = readDay();
         printEventNotice();
         printMessage(ORDER_PROMPT_MESSAGE);
-        this.order = readOrder();
+        Verifier<Order> runtimeVerifier = new RuntimeVerifier();
+        this.order = readOrder(InputView::tryReadOrder, runtimeVerifier);
         this.settlementSystem = new SettlementSystem(day, order);
     }
 
     public void run() {
         renderResult();
+    }
+
+    private Order readOrder(Supplier<Order> orderSupplier, Verifier<Order> verifier) {
+        return repeatUntilValid(orderSupplier, verifier);
+    }
+
+    private Order repeatUntilValid(Supplier<Order> orderSupplier, Verifier<Order> verifier) {
+        while (true) {
+            try {
+                Order order = orderSupplier.get();
+                verifier.check(order);
+                return order;
+            } catch (IllegalStateException e) {
+                printMessage(e.getMessage());
+            }
+        }
     }
 
     private void renderResult() {
@@ -42,18 +58,5 @@ public class PlannerSystem {
     private void renderPlannerResult() {
         printResultStartMessage(day);
         printOrderList(order);
-    }
-
-    private Order readOrder() {
-        Order order;
-        while (true) {
-            try {
-                order = InputView.tryReadOrder();
-                runtimeVerifier.check(order);
-                return order;
-            } catch (IllegalStateException e) {
-                printMessage(e.getMessage());
-            }
-        }
     }
 }
