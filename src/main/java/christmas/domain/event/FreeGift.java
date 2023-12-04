3607e6c744af850179107e9afa2ef2cfe69925fb
Hoo-sung <gntjd135@gmail.com>
2023-12-05 01:12:34 +0900

    feat(FreeGift): FreeGift 클래스 생성

---
+++ src/main/java/christmas/domain/event/FreeGift.java
@@ -0,0 +1,26 @@
+package christmas.domain.event;
+
+import christmas.domain.Menu;
+
+public class FreeGift {
+
+    private final Menu freeGift;
+    private final int quantity;
+
+    public FreeGift(Menu freeGift, int quantity) {
+        this.freeGift = freeGift;
+        this.quantity = quantity;
+    }
+
+    public int calculateBenefitPrice(){
+        return freeGift.getPrice() * quantity;
+    }
+
+    public String getGiftName() {
+        return freeGift.getName();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
