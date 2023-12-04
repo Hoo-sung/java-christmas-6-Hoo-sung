099bb0bb1446aee8688b6b246cd33a86eb48df17
Hoo-sung <gntjd135@gmail.com>
2023-12-05 01:53:57 +0900

    feat(EventService): 이벤트들을 서비스하는 EventService 구현

---
+++ src/main/java/christmas/service/event/EventService.java
@@ -0,0 +1,36 @@
+package christmas.service.event;
+
+import christmas.domain.event.DiscountRecord;
+import christmas.domain.event.FreeGift;
+import christmas.domain.order.Order;
+import christmas.service.event.discount.DiscountEventService;
+import christmas.service.event.gift.FreeGiftEventService;
+
+import java.util.Optional;
+
+public class EventService {
+    private static final int APPLICABLE_MIN_TOTAL_PRICE = 10000;
+
+    private DiscountEventService discountEventService;
+
+    private FreeGiftEventService freeGiftEventService;
+
+    public EventService(DiscountEventService discountEventService, FreeGiftEventService freeGiftEventService) {
+        this.discountEventService = discountEventService;
+        this.freeGiftEventService = freeGiftEventService;
+    }
+
+    public Optional<DiscountRecord> applyDiscountEvent(final Order order) {
+        if (order.getTotalOrderPrice() >= APPLICABLE_MIN_TOTAL_PRICE) {
+            return Optional.of(discountEventService.calculateDiscountEvent(order));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<FreeGift> applyFreeGiftEvent(final Order order) {
+        if (order.getTotalOrderPrice() >= APPLICABLE_MIN_TOTAL_PRICE) {
+            return freeGiftEventService.calculateGiftEvent(order.getTotalOrderPrice());
+        }
+        return Optional.empty();
+    }
+}
