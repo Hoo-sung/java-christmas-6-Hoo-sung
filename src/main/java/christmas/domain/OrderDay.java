ef9bf94bef416ee150de857719198e9ab043bd02
Hoo-sung <gntjd135@gmail.com>
2023-12-04 21:42:57 +0900

    feat(OrderDay): OrderDay 클래스 생성

---
+++ src/main/java/christmas/domain/OrderDay.java
@@ -0,0 +1,75 @@
+package christmas.domain;
+
+import static christmas.system.Constant.CHRISTMAS;
+import static christmas.system.Constant.FIRST_DAY_OF_MONTH;
+
+public class OrderDay {
+
+    private static final int WEEK_LENGTH = 7;
+    private final int day;
+
+    public OrderDay(int day) {
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public boolean isChristmasSeason() {
+        if (day >= FIRST_DAY_OF_MONTH && day <= CHRISTMAS) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isWeekday() {
+        if (!(isFriday() || isSaturday())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isWeekend() {
+        if (isFriday() || isSaturday()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSpecialDay() {
+        if (isSunday() || isChristmas()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFriday() {
+        if (day % WEEK_LENGTH == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSaturday() {
+        if (day % WEEK_LENGTH == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSunday() {
+        if (day % WEEK_LENGTH == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isChristmas() {
+        if (day == CHRISTMAS) {
+            return true;
+        }
+        return false;
+    }
+
+}
