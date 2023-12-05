2ecb5ec0ba422addc7acd1f3cd3c6b28b988196a
Hoo-sung <gntjd135@gmail.com>
2023-12-05 11:15:01 +0900

    feat(OrderRequestMapper): 입력받은 String을 OrderRequest로 매핑해주는 Mapper 생성

---
+++ src/main/java/christmas/util/OrderRequestMapper.java
@@ -0,0 +1,26 @@
+package christmas.util;
+
+import christmas.dto.request.OrderItemRequest;
+import christmas.dto.request.OrderRequest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OrderRequestMapper {
+
+    private static final String menuSeparator = ",";
+    private static final String menuAndQuantitySeparator = "-";
+    private static final int menuAndQuantityLength = 2;
+    private static final int menuIndex = 0;
+    private static final int quantityIndex = 1;
+
+    public static OrderRequest fromMenuForm(String menuForm, int day) {
+        List<OrderItemRequest> orderItemRequests = Arrays.stream(menuForm.split(menuSeparator))
+                .map(nameAndCount -> nameAndCount.split(menuAndQuantitySeparator))
+                .filter(parts -> parts.length == menuAndQuantityLength)
+                .map(parts -> new OrderItemRequest(parts[menuIndex], Integer.parseInt(parts[quantityIndex])))
+                .toList();
+
+        return new OrderRequest(orderItemRequests,day);
+    }
+}
